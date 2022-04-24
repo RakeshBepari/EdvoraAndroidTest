304a4841e792b092e9f0c4398ace1e86a7537019
Rakesh <rakeshbepari.rb@gmai.com>
2022-04-24 12:34:22 +0530

    Branch for disabling Coil caching to show different images with same url in the rides list

--- app/src/main/java/com/example/mobiletestrakesh/MobileTestApplication.kt
+++ app/src/main/java/com/example/mobiletestrakesh/MobileTestApplication.kt
@@ -1,7 +1,21 @@
 package com.example.mobiletestrakesh
 
 import android.app.Application
+import coil.ImageLoader
+import coil.ImageLoaderFactory
+import coil.disk.DiskCache
+import coil.memory.MemoryCache
+import coil.request.CachePolicy
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class MobileTestApplication:Application()
+class MobileTestApplication:Application(),ImageLoaderFactory{
+    override fun newImageLoader(): ImageLoader {
+        return ImageLoader.Builder(this)
+            .diskCachePolicy(CachePolicy.DISABLED)
+            .memoryCachePolicy(CachePolicy.DISABLED)
+            .build()
+    }
+
+
+}
